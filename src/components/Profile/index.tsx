2b72ff63a69252dcb05183dcf124f3c0a4661d0d
Amos Magalhaes <amosmagalhaes@macair.local>
2025-12-15 19:32:16 -0300

    change image profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,13 +1,18 @@
-import { ProfileContainer, ImgProfile } from './styles'
+import { ProfileContainer, ImgProfile } from "./styles";
 
-import ImageProfile from  '../../assets/ams-profile.png'
+import ImageProfile from "../../assets/ams-profile.jpeg";
 
 export function Profile() {
-    return (
-        <ProfileContainer>
-            <ImgProfile src={ImageProfile} />
-            <h1>Amós Magalhães</h1>
-            <p>Data Engineer <a href="https://db.tec.br/" target="_blank" rel="noopener noreferrer">@DB Server · Design & Build</a></p>
-        </ProfileContainer>
-    )
+  return (
+    <ProfileContainer>
+      <ImgProfile src={ImageProfile} />
+      <h1>Amós Magalhães</h1>
+      <p>
+        Data Engineer{" "}
+        <a href="https://db.tec.br/" target="_blank" rel="noopener noreferrer">
+          @DB Server · Design & Build
+        </a>
+      </p>
+    </ProfileContainer>
+  );
 }
